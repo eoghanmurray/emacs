21ec48986afbb5582d7884a18ade2b3edb199792
Eoghan Murray <eoghan@getthere.ie>
2017-08-14 15:31:41 +0100

    Very important command - cycle through recent edits

--- keyboard-editing.el
+++ keyboard-editing.el
@@ -1,5 +1,9 @@
 (global-set-key [f1] 'dabbrev-expand)
 
+(load-file "lisp/goto-last-change.el")
+(require 'goto-last-change)
+(global-set-key [f5] 'goto-last-change)
+
 (defun delete-whitespace ()
   "Delete characters from point up to next non-whitespace char"
   (interactive)
