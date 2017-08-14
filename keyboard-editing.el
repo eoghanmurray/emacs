a8d0c680a977577a7ce85f66d59aee9d241fbb28
Eoghan Murray <eoghan@getthere.ie>
2017-08-14 15:29:38 +0100

    Handy to be able to delete whitespace up to first char e.g. after a paste

--- keyboard-editing.el
+++ keyboard-editing.el
@@ -1,1 +1,10 @@
 (global-set-key [f1] 'dabbrev-expand)
+
+(defun delete-whitespace ()
+  "Delete characters from point up to next non-whitespace char"
+  (interactive)
+  (let ((here (point)))
+    (skip-syntax-forward "-")
+    (if (/= (point) here)
+	(delete-region (point) here))))
+(global-set-key [(meta s)] 'delete-whitespace)
